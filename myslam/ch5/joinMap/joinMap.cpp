555bc0c5c9750200623234634e3c8e48a4ca8650
Leo Ju <ArmyChu1989@gmail.com>
2018-10-20 02:03:48 +0800

    Create joinMap project to generate pointcloud

---
+++ myslam/ch5/joinMap/joinMap.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
+
+int main(int argc, char** argv)
+{
+  vector<Mat> colorImages, depthImages;
+  vector<Isometry3d> poses;	// Poses of camera
+  
+  
+}
