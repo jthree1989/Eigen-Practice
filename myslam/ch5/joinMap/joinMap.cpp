86ff1eb1fda5130df43597050dddbd2bec380230
Leo Ju <ArmyChu1989@gmail.com>
2018-10-23 22:37:23 +0300

    Add color and depth images and read pose from file

--- myslam/ch5/joinMap/joinMap.cpp
+++ myslam/ch5/joinMap/joinMap.cpp
@@ -25,9 +25,21 @@ int main(int argc, char** argv)
   }
   
   for(auto i =0; i < 5; i++){
-    format fmt("./%s%d.%s");
+    boost::format fmt("./%s%d.%s");
     colorImages.push_back(imread((fmt%"color"%(i+1)%"png").str()));
-    depthImages.push_back(imread((fmt%"depth"%(i+1)%"png").str(), -1)); // Read raw data
+    depthImages.push_back(imread((fmt%"depth"%(i+1)%"png").str(), -1)); // Read raw data with second para(-1)
+    
+    double data[7] = {0};
+    for(auto& d:data)	// Use reference not copy value  
+      fin >> d;
+    
+    Quaterniond q(data[6],data[3], data[4], data[5]);
+    Isometry3d T(q);
+    T.pretranslate<double>(Vector3d(data[0], data[1], data[2]));
+    poses.push_back(T);
   }
+  
+  
+  return 0;
   
 }
